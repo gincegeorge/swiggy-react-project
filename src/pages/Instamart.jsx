78392c9a33e75da0a78db4cd2b7718f924f9fd3f
Gince George <gincegeo@gmail.com>
2023-04-11 14:17:43 +0530

    props

--- src/pages/Instamart.jsx
+++ src/pages/Instamart.jsx
@@ -1,17 +1,71 @@
+import { useState } from "react";
+
+const Section = ({ title, desc, isVisible, setIsVisible, hideSection }) => {
+  return (
+    <div className="my-5 bg-slate-600 text-[#fff] p-5 border text-left">
+      <h3 className="font-bold text-xl mb-3">{title}</h3>
+      <div className="my-2">
+        {isVisible ? (
+          <>
+            <button onClick={() => hideSection()} className="m-1 underline">
+              Hide
+            </button>
+            <p>{desc}</p>
+          </>
+        ) : (
+          <button onClick={() => setIsVisible()} className="m-1 underline">
+            Show
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
 const Instamart = () => {
+  const [visibleSection, setVisibleSection] = useState("about");
   return (
     <>
-      <div className="container">
-        <h1>Instamart page </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+      <div className="container mx-auto max-w-7xl p-10 text-center">
+        <Section
+          title="About section"
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam"
+          isVisible={visibleSection === "about"}
+          setIsVisible={() => setVisibleSection("about")}
+          hideSection={() => setVisibleSection("")}
+        />
+        <Section
+          title="Team section"
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam"
+          isVisible={visibleSection === "team"}
+          setIsVisible={() => setVisibleSection("team")}
+          hideSection={() => setVisibleSection("")}
+        />
+        <Section
+          title="Careers section"
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam"
+          isVisible={visibleSection === "careers"}
+          setIsVisible={() => setVisibleSection("careers")}
+          hideSection={() => setVisibleSection("")}
+        />
+        <div className="container">
+          <h1>Instamart page </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     </>
   );
